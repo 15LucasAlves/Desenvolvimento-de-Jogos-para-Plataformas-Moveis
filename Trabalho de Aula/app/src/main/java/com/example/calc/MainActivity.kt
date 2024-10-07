6ede254662cf27b2dc267459cc44c8c4c4e9f0d1
Layfyy <150027558+15LucasAlves@users.noreply.github.com>
2024-10-07 14:09:33 +0100

    comit

--- Trabalho de Aula/app/src/main/java/com/example/calc/MainActivity.kt
+++ Trabalho de Aula/app/src/main/java/com/example/calc/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalcTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    calcmain(modifier = Modifier.padding(innerPadding));
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalcTheme {
-        Greeting("Android")
     }
 }
--- Trabalho de Aula/app/src/main/java/com/example/calc/ui/theme/calcmain.kt
+++
@@ -1,2 +0,0 @@
-package com.example.calc.ui.theme
-
